82883aeccf5ad3acb43e2715d26c894390527985
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-01-28 17:42:22 +0000

    First commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="font-sans text-accent">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in quo dicta rem totam, quae maxime iusto reiciendis porro praesentium dolor corrupti est nulla delectus? Nam consequatur illo accusantium minus?</p>
+    </div>
+  );
+}
