66ee2a899faaa8df6247fe53d2bce4fde32096df
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-02-12 19:48:52 +0000

    Updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,13 +4,36 @@ const page = () => {
   return (
     <section className='pt-32 font-sans'>
       <Wrapper>
-        <div className="w-full">
-          <p className="text-h3 leading-tight text-center">224 Reels is a personal archive of moving images and "experiments" that I make in my free time.</p>
+        <div className="w-full pt-8">
           <div className="bg-red-500 aspect-square my-10" />
-          <p className="text-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta perspiciatis quidem corrupti aliquam consequuntur placeat excepturi, adipisci consectetur praesentium in vel facilis consequatur eveniet delectus omnis, sequi reiciendis pariatur?</p>
+          <p className="text-h3 md:text-h2 lg:text-7xl font-medium leading-[1.1] text-center max-w-7xl w-full mx-auto">224 Reels is a personal archive of moving images and "experiments" that I make in my free time.</p>
+          <p className="text-body md:text-h3 w-full max-w-3xl text-center mx-auto">This space exists to document and showcase my "shots" —  composition, and the feeling that lives between frames. Some of these "shots" are polished. Others are just fragments, tests, or ideas in motion.</p>
+
+          <div className="pt-20 md:pt-32 space-y-12">
+            <p className="text-h3 md:text-h2 lg:text-7xl font-medium leading-[1.1] text-center max-w-7xl w-full mx-auto">This archive grows alongside the work — a record of process, iteration, and evolving perspective.</p>
+            <p className="text-body md:text-h3 w-full max-w-3xl text-center mx-auto">This space exists to document and showcase my "shots" —  composition, and the feeling that lives between frames. Some of these "shots" are polished. Others are just fragments, tests, or ideas in motion.</p>
+
+          </div>
         </div>
+
       </Wrapper>
     </section>
+    // <section className='pt-32 font-sans'>
+    //   <Wrapper>
+    //     <div className="w-full pt-8">
+    //       <p className="text-h3 md:text-h2 lg:text-7xl font-medium leading-[1.1] text-center max-w-7xl w-full mx-auto">224 Reels is a personal archive of moving images and "experiments" that I make in my free time.</p>
+    //       <div className="bg-red-500 aspect-square my-10" />
+    //       <p className="text-body md:text-h3 w-full max-w-3xl text-center mx-auto">This space exists to document and showcase my "shots" —  composition, and the feeling that lives between frames. Some of these "shots" are polished. Others are just fragments, tests, or ideas in motion.</p>
+
+    //       <div className="pt-20 md:pt-32 space-y-12">
+    //         <p className="text-h3 md:text-h2 lg:text-7xl font-medium leading-[1.1] text-center max-w-7xl w-full mx-auto">This archive grows alongside the work — a record of process, iteration, and evolving perspective.</p>
+    //         <p className="text-body md:text-h3 w-full max-w-3xl text-center mx-auto">This space exists to document and showcase my "shots" —  composition, and the feeling that lives between frames. Some of these "shots" are polished. Others are just fragments, tests, or ideas in motion.</p>
+
+    //       </div>
+    //     </div>
+
+    //   </Wrapper>
+    // </section>
   )
 }
 
