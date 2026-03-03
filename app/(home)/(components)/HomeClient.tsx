e8b8b9a36f8b3d411bd6520aa0f4573d0da861cc
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-03 16:45:36 +0000

    Initial

--- app/(home)/(components)/HomeClient.tsx
+++ app/(home)/(components)/HomeClient.tsx
@@ -7,92 +7,25 @@ import MobileScreen from "./MobileScreen";
 import classNames from "classnames";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import Wrapper from "@/app/components/Wrapper";
 
 export default function Home() {
-  const [currentReel, setCurrentReel] = useState(0);
-  const [hoveredReel, setHoveredReel] = useState<number | null>(null);
-  const imgContainer = useRef<HTMLDivElement>(null);
-
-  const slicedReels = reelsData.slice(3, 8);
-
-  const handleHover = (idx: number) => {
-    setHoveredReel(idx);
-  }
-
-  const handleMouseLeave = () => {
-    setHoveredReel(null);
-  }
-
-  const handleClick = (idx: number) => {
-    setCurrentReel(idx);
-    setHoveredReel(null);
-  }
-
-  const displayedReel = hoveredReel !== null ? hoveredReel : currentReel;
-
-  useGSAP(() => {
-    if (!imgContainer.current) return;
-
-    gsap.set(imgContainer.current, {
-      opacity: 0.3,
-      scale: 1.01,
-    })
-
-    gsap.to(imgContainer.current, {
-      opacity: 1,
-      scale: 1,
-      duration: 0.6,
-      ease: 'power2.inOut',
-    })
-  }, [displayedReel])
 
   return (
-    <section className="w-full h-dvh">
-      <div className=" h-full hidden lg:block">
-        <div className="w-full h-full fixed inset-0">
-          <div ref={imgContainer} className="relative w-full h-full">
+    <section className="flex items-center justify-center h-screen w-full">
+      <Wrapper >
+
+        <div className="w-full h-full items-center justify-center flex">
+          <div className="relative w-92 h-62 md:w-120 md:h-75  bg-black">
             <Image
-              src={slicedReels[displayedReel].img}
-              alt={slicedReels[displayedReel].title}
+              src="/images/img-7.webp"
+              alt="image 1"
               fill
-              className="object-cover object-center"
+              className="object-center object-contain"
             />
           </div>
-          <div className="absolute inset-0 bg-black opacity-20"></div>
         </div>
-
-        <div className="w-full h-full relative flex items-end justify-center pb-6">
-          <div className="hidden lg:block">
-            <ul className="text-background flex max-w-lg flex-wrap items-center justify-center w-full gap-0 md:gap-1">
-              {slicedReels.map((reel, idx) => {
-                return (
-                  <li key={idx} className="flex gap-1 items-center">
-                    <span
-                      onMouseEnter={() => handleHover(idx)}
-                      onMouseLeave={handleMouseLeave}
-                      onClick={() => handleClick(idx)}
-                      className={classNames(
-                        'cursor-pointer text-body md:text-xl hover:text-grey-400 duration-300 font-sans  tracking-wide',
-                        currentReel === idx && 'text-grey-200/70'
-                      )}
-                    >
-                      {reel.title}
-                    </span>
-                    {idx < slicedReels.length - 1 && (
-                      <span className="font-medium text-body md:text-xl">
-                        /
-                      </span>
-                    )}
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="block lg:hidden h-full">
-        <MobileScreen />
-      </div>
+      </Wrapper>
     </section>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${clashGrotesk.variable} antialiased`}
       >
         <SmoothScroll>
-          <Nav />
+          {/* <Nav /> */}
           {children}
         </SmoothScroll>
       </body>
