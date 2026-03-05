f3131db509e5eda44d033d1bc195cfaac10d6790
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-05 16:49:46 +0000

    Changed home layout

--- app/(home)/(components)/HomeClient.tsx
+++ app/(home)/(components)/HomeClient.tsx
@@ -1,57 +1,32 @@
 'use client'
-import { useRef, useState, useEffect } from "react";
-import { VideoAsset } from "@/app/utils/types";
-import { reelsData } from "@/app/utils/data";
-import Image from "next/image";
-import MobileScreen from "./MobileScreen";
-import classNames from "classnames";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import Wrapper from "@/app/components/Wrapper";
+
+import { timelineEntries } from "@/app/utils/data";
 
 export default function Home() {
 
-  const slicedReels = reelsData.slice(0, 3);
-  const cardsRef = useRef<(HTMLDivElement | null)[]>([]);
-  useGSAP(() => {
-    cardsRef.current.forEach((card, i) => {
-      if (!card) return;
-      gsap.set(card, {
-        yPercent: i * -10,
-        scale: 1 - i * 0.05,
-        zIndex: slicedReels.length - i,
-        transformOrigin: "center center",
-        transformPerspective: 800,
-        rotationX: i * 2,
-      });
-    });
-  }, []);
-
   return (
-    <section className="flex items-center justify-center h-screen w-full">
-      <Wrapper>
-        <div className="w-full h-full flex flex-col items-center justify-center gap-4">
-          <div className="w-92 lg:w-120 xl:w-xl mx-auto" style={{ aspectRatio: "16/9", position: "relative" }}>
-            {slicedReels.map((reel, i) => (
-              <div
-                key={i}
-                ref={(el) => { cardsRef.current[i] = el; }}
-                className="absolute inset-0 w-full h-full"
-              >
-                <div className="relative w-full h-full">
-                  <Image
-                    src={reel.img}
-                    alt="image 1"
-                    fill
-                    className="object-cover object-center"
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-          <span className="text-sm font-sans font-medium">Video title 1</span>
-        </div>
-      </Wrapper>
+    <section className="w-full min-h-screen fill">
+      <ul>
+        {timelineEntries.map((entry, i) => {
+          return (
+            <div key={i} className="h-screen p-5">
+              <li className="relative h-full w-full">
+                {entry.images.map((image, j) => {
+                  return (
+                    <div key={j} className={`absolute size-full ${image.position} ${image.width}`}>
+                      <img
+                        className={`absolute ${image.aspectRatio} object-cover`}
+                        src={image.src}
+                        alt={image.alt}
+                      />
+                    </div>
+                  )
+                })}
+              </li>
+            </div>
+          )
+        })}
+      </ul>
     </section>
   );
 }
--- app/utils/data.ts
+++ app/utils/data.ts
@@ -71,3 +71,82 @@ export const reelsData = [
     type: "reel"
   }
 ];
+
+export const timelineEntries = [
+  {
+    id: "entry-2024",
+    year: "2024",
+    images: [
+      {
+        position: "left-[4vw] top-[6vw]",
+        mobilePosition: "left-0 top-[6vw]",
+        width: "w-[34vw]",
+        aspectRatio: "aspect-video",
+        src: "/images/img-1.webp",
+        alt: "Image 1",
+      },
+      {
+        position: "right-[4vw] top-[38vw]",
+        mobilePosition: "right-0 top-[60vw]",
+        width: "w-[26vw]",
+        aspectRatio: "aspect-square",
+        src: "/images/img-2.webp",
+        alt: "Image 2",
+      },
+      {
+        position: "left-[10vw] top-[74vw]",
+        mobilePosition: "left-0 top-[118vw]",
+        width: "w-[36vw]",
+        aspectRatio: "aspect-[4/3]",
+        src: "/images/img-3.webp",
+        alt: "Image 3",
+      },
+      {
+        position: "right-[8vw] top-[108vw]",
+        mobilePosition: "right-0 top-[178vw]",
+        width: "w-[30vw]",
+        aspectRatio: "aspect-[3/2]",
+        src: "/images/img-4.webp",
+        alt: "Image 4",
+      },
+    ],
+  },
+  {
+    id: "entry-2025",
+    year: "2025",
+    images: [
+      {
+        position: "left-[6vw] top-[6vw]",
+        mobilePosition: "left-0 top-[6vw]",
+        width: "w-[40vw]",
+        aspectRatio: "aspect-[16/10]",
+        src: "/images/img-6.webp",
+        alt: "Image 6",
+      },
+      {
+        position: "right-[3vw] top-[40vw]",
+        mobilePosition: "right-0 top-[62vw]",
+        width: "w-[26vw]",
+        aspectRatio: "aspect-[3/4]",
+        src: "/images/img-7.webp",
+        alt: "Image 7",
+      },
+      {
+        position: "left-[16vw] top-[78vw]",
+        mobilePosition: "left-[6vw] top-[122vw]",
+        width: "w-[22vw]",
+        aspectRatio: "aspect-square",
+        src: "/images/img-8.webp",
+        alt: "Image 8",
+      },
+      {
+        position: "right-[10vw] top-[112vw]",
+        mobilePosition: "right-[4vw] top-[182vw]",
+        width: "w-[24vw]",
+        aspectRatio: "aspect-[9/16]",
+        src: "/images/img-5.webp",
+        alt: "Image 5",
+      },
+    ],
+  },
+];
